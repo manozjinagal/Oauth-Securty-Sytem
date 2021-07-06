81f08d244e31f043ece91456a88892866a522e29
manozjinagal <manozjinagal@gmail.com>
2021-07-06 19:56:31 +0530

    src

---
+++ Minor Project 2/src/main/java/com/NITKKR/oauth2/project/Application.java
@@ -0,0 +1,15 @@
+package com.NITKKR.oauth2.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+
+@SpringBootApplication(exclude = JmxAutoConfiguration.class)
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ Minor Project 2/src/main/java/com/NITKKR/oauth2/project/repository/CompanyRepository.java
@@ -0,0 +1,22 @@
+package com.NITKKR.oauth2.project.repository;
+
+import com.NITKKR.oauth2.project.model.Company;
+
+import java.util.List;
+
+public interface CompanyRepository {
+
+    Company find(Long id);
+
+    Company find(String name);
+
+    List<Company> findAll();
+
+    void create(Company company);
+
+    Company update(Company company);
+
+    void delete(Long id);
+
+    void delete(Company company);
+}
